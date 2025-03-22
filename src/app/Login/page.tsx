90058ec01c272ad48f7ae2a498863d380bf95c2d
kronosei <laurenceavillaruz@gmail.com>
2025-03-21 22:14:37 -0400

    Made super basic homepage

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout"
+
+export default function Login() {
+    return (
+        <Layout>
+            <div>
+                <header>LOGIN PAGE</header>
+            </div>
+        </Layout>
+    )
+}
