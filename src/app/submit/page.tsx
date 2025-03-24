dfd23fce13cb84cf5d62856bf0e88580e241a2f8
kronosei <laurenceavillaruz@gmail.com>
2025-03-24 19:55:02 -0400

    Changing directory names

---
+++ src/app/submit/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout"
+
+export default function Submit() {
+    return (
+        <Layout>
+            <div>
+                <header>Form Submission Page</header>
+            </div>
+        </Layout>
+    )
+}
